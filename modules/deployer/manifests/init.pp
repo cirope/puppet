6c2283b638dfab7fd141a67b3cbff7420e2d4645
Franco Catena <francocatena@gmail.com>
2013-10-21 20:40:52 -0300

    Better vim config to deployer

--- modules/deployer/manifests/init.pp
+++ modules/deployer/manifests/init.pp
@@ -1,13 +1,14 @@
 class deployer {
-  $user = 'deployer'
-  $home = "/home/$user"
+  $user  = 'deployer'
+  $group = 'www-data'
+  $home  = "/home/$user"
 
   user { $user:
     ensure     => present,
     managehome => true,
     home       => $home,
     shell      => '/bin/zsh',
-    gid        => 'www-data',
+    gid        => $group,
     groups     => [ 'admin' ],
     require    => Group['admin']
   }
--- modules/deployer/manifests/vim.pp
+++ modules/deployer/manifests/vim.pp
@@ -1,8 +1,21 @@
 class vim {
-  file { '.vimrc':
-    path    => "$deployer::home/.vimrc",
+  file { "$deployer::home/.vimrc":
     ensure  => file,
+    owner   => $deployer::user,
+    group   => $deployer::group,
     require => Package['vim-nox'],
     source  => "puppet:///modules/deployer/vim/.vimrc"
+  }
+
+  file { "$deployer::home/.vim/backup":
+    ensure => directory,
+    owner  => $deployer::user,
+    group  => $deployer::group
+  }
+
+  file { "$deployer::home/.vim/undodir":
+    ensure => directory,
+    owner  => $deployer::user,
+    group  => $deployer::group
   }
 }
