e2d2c39f7a0667bff8e23d47fcc687dc207bcd41
Franco Catena <francocatena@gmail.com>
2013-10-22 01:55:38 -0300

    PostgreSQL service suscribed to changes in config =)

--- modules/services/manifests/postgresql.pp
+++ modules/services/manifests/postgresql.pp
@@ -1,11 +1,13 @@
 class postgresql {
   service { 'postgresql':
-    enable  => true,
-    ensure  => running,
-    require => Package['postgresql']
+    enable    => true,
+    ensure    => running,
+    require   => Package['postgresql'],
+    subscribe => File['postgresql.conf']
   }
 
-  file { '/etc/postgresql/9.1/main/postgresql.conf':
+  file { 'postgresql.conf':
+    path    => '/etc/postgresql/9.1/main/postgresql.conf',
     ensure  => file,
     require => Package['postgresql'],
     content => template('services/postgresql.conf.erb')
