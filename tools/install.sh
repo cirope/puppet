7878e38349f7b2f99ca83084752e864fd2a9f210
Franco Catena <francocatena@gmail.com>
2013-10-22 00:39:04 -0300

    Source files changed to sh files =)

--- tools/install.sh
+++ tools/install.sh
@@ -2,12 +2,12 @@ if [ ! -d /etc/puppet ]; then
   apt-get install -y git-core
   git clone https://github.com/cirope/puppet.git /etc/puppet
 
-  source /etc/puppet/tools/install_puppet.sh
-  source /etc/puppet/tools/vendor_modules.sh
+  sh /etc/puppet/tools/install_puppet.sh
 else
   cd /etc/puppet
   git clean -f -d
   git pull
 fi
 
+sh /etc/puppet/tools/vendor_modules.sh
 puppet apply /etc/puppet/manifests/site.pp
