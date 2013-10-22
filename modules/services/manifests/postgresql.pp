91ee04aab32f24bac8c8b1da0b7f4fb8c485a7e9
Franco Catena <francocatena@gmail.com>
2013-10-22 11:00:08 -0300

    Change permissions and owners in file templates

--- modules/services/manifests/postgresql.pp
+++ modules/services/manifests/postgresql.pp
@@ -2,14 +2,17 @@ class postgresql {
   service { 'postgresql':
     enable    => true,
     ensure    => running,
-    require   => Package['postgresql'],
-    subscribe => File['postgresql.conf']
+    require   => Package['postgresql']
   }
 
   file { 'postgresql.conf':
     path    => '/etc/postgresql/9.1/main/postgresql.conf',
     ensure  => file,
+    owner   => postgres,
+    group   => postgres,
+    mode    => 0644,
+    content => template('services/postgresql.conf.erb'),
     require => Package['postgresql'],
-    content => template('services/postgresql.conf.erb')
+    notify  => Service['postgresql']
   }
 }
--- modules/system/manifests/init.pp
+++ modules/system/manifests/init.pp
@@ -1,6 +1,9 @@
 class system {
   file { '/etc/sysctl.conf':
     ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
     content => template('system/sysctl.conf.erb')
   }
 }
