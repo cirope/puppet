a69397c8f0f9a42ac74e46ab1794022686846dbe
Franco Catena <francocatena@gmail.com>
2013-10-22 15:31:34 -0300

    SSH RSA improved

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,11 +1,12 @@
 class ssh {
   $franco_rsa = 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD34cQ6DpxqkEvivkW07GnwlEeCAtnwxMhZs7TreWP20cDTOqAtoKN1LhIBAhu7OUyHJw2kof6u5t3ObTJJDGMoqMBZgp7y8vD1UjPimgD4NsLTx0nwU/K5Mn/gL64Dab8LEk+x9rpYsxN/48AXvElIhjaGOUR0Iemif20uqopgOxq9sO6PeWpYRU1o8ox286gIF4LLoeOmlUmwNtko9o/v+LFPRXwoj8hUNU2JzlI2AuYpEiYXDGIdqN8nvoIFjw7ZvuVSCEw7YI12itP4xkL52F6GnibnLCAAM3sheFwvW/Ftx02v1iOxaJ2ZRxSTLwfh54WCs6QSQLTq3ftjmncx'
 
-  ssh_authorized_key { 'franco_deployer':
-    ensure => present,
-    key    => $franco_rsa,
-    type   => rsa,
-    user   => deployer
+  ssh_authorized_key { "franco_$deployer::user":
+    ensure  => present,
+    key     => $franco_rsa,
+    type    => rsa,
+    user    => $deployer::user,
+    require => User[$deployer::user]
   }
 
   ssh_authorized_key { 'franco_root':
