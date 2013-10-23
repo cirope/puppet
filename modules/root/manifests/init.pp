71ceb26e988a1d0a85bb6db2fb735e66b9145d3a
Franco Catena <francocatena@gmail.com>
2013-10-23 14:17:56 -0300

    No more update LANG in root

--- modules/root/manifests/init.pp
+++ modules/root/manifests/init.pp
@@ -5,15 +5,6 @@ class root {
     ensure  => file,
     owner   => root,
     group   => root,
-    source  => 'puppet:///modules/root/.profile',
-    require => Exec['update-locale']
-  }
-
-  exec { 'root-profile-refresh':
-    command     => "bash -c 'source $profile'",
-    user        => root,
-    refreshonly => true,
-    subscribe   => File[$profile],
-    require     => File[$profile]
+    source  => 'puppet:///modules/root/.profile'
   }
 }
