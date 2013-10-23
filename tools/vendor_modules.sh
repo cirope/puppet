03252bb214ee7c7d518a2503bf9b33825c02886d
Franco Catena <francocatena@gmail.com>
2013-10-23 15:50:57 -0300

    Another name change for postgres module =)

--- tools/vendor_modules.sh
+++ tools/vendor_modules.sh
@@ -1,7 +1,7 @@
-if [ ! -d /etc/puppet/rbenv ]; then
+if [ ! -d /etc/puppet/modules/rbenv ]; then
   puppet module install alup/rbenv
 fi
 
-if [ ! -d /etc/puppet/postgresql ]; then
-  puppet module install puppetlabs/postgresql 
+if [ ! -d /etc/puppet/modules/postgresql_puppet ]; then
+  puppet module install --target-dir /etc/puppet/modules/postgresql_puppetlabs puppetlabs/postgresql 
 fi
