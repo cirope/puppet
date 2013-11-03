32ed1bf2396da0d3d752c1349ac612cf2af8616e
Franco Catena <francocatena@gmail.com>
2013-11-03 20:48:25 -0300

    No more package source in nginx

--- modules/nginx_conf/manifests/init.pp
+++ modules/nginx_conf/manifests/init.pp
@@ -1,6 +1,5 @@
 class nginx_conf {
   class { 'nginx':
-    package_source  => 'passenger',
     http_cfg_append => {
       'passenger_root' => '/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini',
     }
