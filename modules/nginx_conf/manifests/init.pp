de2b20bbc02143c8158228f8535acb81e9dc8e03
Franco Catena <francocatena@gmail.com>
2013-11-03 20:41:14 -0300

    Add vhost config to Nginx

--- modules/nginx_conf/manifests/init.pp
+++ modules/nginx_conf/manifests/init.pp
@@ -1,3 +1,10 @@
 class nginx_conf {
-  class { 'nginx': }
+  class { 'nginx':
+    package_source  => 'passenger',
+    http_cfg_append => {
+      'passenger_root' => '/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini',
+    }
+  }
+
+  include vhost
 }
