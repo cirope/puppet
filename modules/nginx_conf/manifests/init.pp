7db50c7add566836b9148d83c8331ad568da62c1
Franco Catena <francocatena@gmail.com>
2013-11-05 22:54:55 -0300

    Back to puppet nginx module

--- modules/nginx_conf/manifests/init.pp
+++ modules/nginx_conf/manifests/init.pp
@@ -1,9 +1,3 @@
 class nginx_conf {
-  class { 'nginx':
-    http_cfg_append => {
-      'passenger_root' => '/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini',
-    }
-  }
-
-  include vhost
+  class { 'nginx': }
 }
