717bea454ad4a8342c258b5f9ebc12d6f5aa63d8
Franco Catena <francocatena@gmail.com>
2013-11-06 20:38:36 -0300

    Add nginx.conf template

--- modules/nginx_conf/manifests/init.pp
+++ modules/nginx_conf/manifests/init.pp
@@ -1,3 +1,11 @@
 class nginx_conf {
   class { 'nginx': }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    content => template('nginx_conf/nginx.conf.erb')
+  }
 }
