ea4abab5aa5ac0de7ad4dfeef91c7972d4524391
Franco Catena <francocatena@gmail.com>
2013-11-06 22:47:54 -0300

    Notify nginx service when nginx.conf changes...

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -8,12 +8,19 @@ class nginx {
     require => Apt::Ppa['ppa:nginx/stable']
   }
 
+  service { 'nginx':
+    enable  => true,
+    ensure  => running,
+    require => Package['nginx']
+  }
+
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => 0644,
     require => Package['nginx'],
+    notify  => Service['nginx'],
     content => template('nginx/nginx.conf.erb')
   }
 }
