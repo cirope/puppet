d8245fb7132ad19483648e8c0b754e9bd9b263f4
Franco Catena <francocatena@gmail.com>
2013-11-09 00:44:22 -0300

    Unicorn service must be at startup

--- modules/unicorn/manifests/init.pp
+++ modules/unicorn/manifests/init.pp
@@ -1,9 +1,15 @@
 class unicorn {
+  service { 'unicorn':
+    enable  => true,
+    require => File['/etc/init.d/unicorn']
+  }
+
   file { '/etc/init.d/unicorn':
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => 0755,
+    notify  => Service['unicorn'],
     content => template('unicorn/init.sh.erb')
   }
 }
