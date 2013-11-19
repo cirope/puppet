fa9cd97751d840b3f6caa17d872abb06d97cad8b
Franco Catena <francocatena@gmail.com>
2013-11-19 16:07:12 -0300

    Add SSH keys with puppet defined types

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,18 +1,20 @@
 class ssh($keys = []) {
-  $keys.each { |$key, $value|
-    ssh_authorized_key { "${key}_root":
+  define user_key() {
+    ssh_authorized_key { "${name}_root":
       ensure => present,
-      key    => $value,
+      key    => $name,
       type   => rsa,
       user   => root
     }
 
-    ssh_authorized_key { "${key}_$deployer::user":
+    ssh_authorized_key { "${name}_$deployer::user":
       ensure  => present,
-      key     => $value,
+      key     => $name,
       type    => rsa,
       user    => $deployer::user,
       require => User[$deployer::user]
     }
   }
+
+  user_key { $keys: }
 }
