ee1bbe7652a6f566195bc2ca20b14196403e8e50
Franco Catena <francocatena@gmail.com>
2013-11-28 14:16:32 -0300

    Add owncloud data dir

--- modules/owncloud/manifests/init.pp
+++ modules/owncloud/manifests/init.pp
@@ -12,4 +12,12 @@ class owncloud {
     ensure  => present,
     require => Apt::Source['owncloud']
   }
+
+  file { '/var/lib/owncloud':
+    ensure  => directory,
+    owner   => www-data,
+    group   => www-data,
+    mode    => 0755,
+    require => Package['owncloud']
+  }
 }
