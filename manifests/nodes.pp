b5ce18c1059d0687689e475948e27725f6c79081
Franco Catena <francocatena@gmail.com>
2013-12-03 19:23:45 -0300

    Add mawidaqa.com config

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,6 +4,12 @@ node 'mawidabp.com', 'mawidabp-dev.com' inherits basenode {
   include unicorn
 }
 
+node 'mawidaqa.com', 'mawidaqa-dev.com' inherits basenode {
+  include deployer
+  include postgresql_conf
+  include unicorn
+}
+
 node 'cirope.com', 'cirope-dev.com' inherits basenode {
   include deployer
   include unicorn
