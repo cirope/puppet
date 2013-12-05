89aab586d76cc593c5642dc5b026cd2d9b32db70
Franco Catena <francocatena@gmail.com>
2013-12-05 17:02:15 -0300

    Add prefix to deployer ssh keys

---
+++ modules/ssh/lib/puppet/parser/functions/prefix_hash_keys.rb
@@ -0,0 +1,30 @@
+module Puppet::Parser::Functions
+  newfunction(:prefix_hash_keys, :type => :rvalue) do |arguments|
+    if arguments.size < 1
+      raise Puppet::ParseError,
+        "prefix_hash_keys(): Wrong number of arguments given (#{arguments.size} for 1)"
+    end
+
+    hash = arguments[0]
+
+    unless hash.is_a?(Hash)
+      raise Puppet::ParseError,
+        "prefix_hash_keys(): expected first argument to be an Hash, got #{hash.inspect}"
+    end
+
+    prefix = arguments[1] if arguments[1]
+
+    if prefix
+      unless prefix.is_a?(String)
+        raise Puppet::ParseError,
+          "prefix_hash_keys(): expected second argument to be a String, got #{prefix.inspect}"
+      end
+    end
+
+    result = {}
+
+    hash.each { |k, v| result["#{prefix}#{k}"] = v }
+
+    result
+  end
+end
