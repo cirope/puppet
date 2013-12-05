67a1106256fde3c2a6e76a8e5d19493ea4c6a75f
Franco Catena <francocatena@gmail.com>
2013-12-05 12:23:39 -0300

    Only install newrelic sysmond if license

--- modules/newrelic/manifests/init.pp
+++ modules/newrelic/manifests/init.pp
@@ -1,32 +1,34 @@
-class newrelic($license = '') {
-  apt::source { 'newrelic':
-    ensure      => present,
-    location    => 'http://apt.newrelic.com/debian/',
-    release     => 'newrelic',
-    repos       => 'non-free',
-    key         => '548C16BF',
-    key_server  => 'subkeys.pgp.net',
-    include_src => false,
-  }
+class newrelic($license = undef) {
+  if $license {
+    apt::source { 'newrelic':
+      ensure      => present,
+      location    => 'http://apt.newrelic.com/debian/',
+      release     => 'newrelic',
+      repos       => 'non-free',
+      key         => '548C16BF',
+      key_server  => 'subkeys.pgp.net',
+      include_src => false,
+    }
 
-  package { 'newrelic-sysmond':
-    ensure  => present,
-    require => Apt::Source['newrelic']
-  }
+    package { 'newrelic-sysmond':
+      ensure  => present,
+      require => Apt::Source['newrelic']
+    }
 
-  service { 'newrelic-sysmond':
-    enable  => true,
-    ensure  => running,
-    require => Package['newrelic-sysmond']
-  }
+    service { 'newrelic-sysmond':
+      enable  => true,
+      ensure  => running,
+      require => Package['newrelic-sysmond']
+    }
 
-  file { '/etc/newrelic/nrsysmond.cfg':
-    ensure  => file,
-    owner   => root,
-    group   => newrelic,
-    mode    => 0640,
-    content => template('newrelic/nrsysmond.cfg.erb'),
-    notify  => Service['newrelic-sysmond'],
-    require => Package['newrelic-sysmond']
+    file { '/etc/newrelic/nrsysmond.cfg':
+      ensure  => file,
+      owner   => root,
+      group   => newrelic,
+      mode    => 0640,
+      content => template('newrelic/nrsysmond.cfg.erb'),
+      notify  => Service['newrelic-sysmond'],
+      require => Package['newrelic-sysmond']
+    }
   }
 }
