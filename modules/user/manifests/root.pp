7b1ee8c3437dae82cc5f84a8de5f393d6edfd2c6
Franco Catena <francocatena@gmail.com>
2013-12-10 16:01:36 -0300

    Add root zsh config 2.0 (adding files)

---
+++ modules/user/manifests/root.pp
@@ -0,0 +1,17 @@
+class user::root {
+  user { 'root':
+    ensure  => present,
+    shell   => '/bin/zsh',
+    require => Package['zsh']
+  }
+
+  user::vimconfig { 'root':
+    user => 'root',
+    home => '/root'
+  }
+
+  user::zsh { 'root':
+    user => 'root'
+    home => '/root'
+  }
+}
