0c6dfa837a9471bbbbcaf4c3387f61ad017e6c64
Franco Catena <francocatena@gmail.com>
2013-12-10 13:43:52 -0300

    Fix rbenv dependency on zshrc

--- modules/user/manifests/deployer/rbenv.pp
+++ modules/user/manifests/deployer/rbenv.pp
@@ -3,7 +3,7 @@ class user::deployer::rbenv {
 
   rbenv::install { 'deployer':
     group   => $user::deployer::group,
-    require => File['zshrc', 'gemrc']
+    require => File["${user::deployer::home}/.zshrc", 'gemrc']
   }
 
   rbenv::compile { $ruby_version:
