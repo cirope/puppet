ad79bd701f6b7013bdab8e457775a528b10e2912
Franco Catena <francocatena@gmail.com>
2013-12-18 16:34:51 -0300

    Divide server concern in roles

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,27 +1,17 @@
-node racknode inherits basenode {
-  include user::deployer
-  include unicorn
+node 'cirope.com' {
 }
 
-node 'cirope.com' inherits racknode {
+node 'libreduca.com' {
 }
 
-node 'libreduca.com' inherits racknode {
-  include pg
+node 'librujo.com' {
 }
 
-node 'librujo.com' inherits racknode {
-  include pg
+node 'mawidabp.com' {
 }
 
-node 'mawidabp.com' inherits racknode {
-  include pg
+node 'mawidaqa.com' {
 }
 
-node 'mawidaqa.com' inherits racknode {
-  include pg
-}
-
-node 'php.libreduca.com' inherits basenode {
-  include php
+node 'php.libreduca.com' {
 }
--- manifests/templates.pp
+++ manifests/templates.pp
@@ -1,22 +1,47 @@
 include vars
 
 class baseclass {
-  if $::osfamily == 'debian' {
-    include apt
-  }
+  include $::osfamily
 
   include system
   include swap
   include packages
   include services
   include user
+}
+
+class server::web {
   include nginx
+}
+
+class server::app {
+  include user::deployer
+  include unicorn
   include newrelic
 }
 
-node basenode {
-  include baseclass
+class server::db {
+  include pg
+}
+
+class server::all {
+  include server::web
+  include server::app
+  include server::db
+}
+
+class server::php {
+  include php
 }
 
 node default {
+  include baseclass
+
+  case $::hostname {
+    web:     { include server::web }
+    app:     { include server::app }
+    db:      { include server::db }
+    php:     { include server::php }
+    default: { include server::all }
+  }
 }
