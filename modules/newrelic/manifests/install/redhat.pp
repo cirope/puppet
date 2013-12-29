e5f72888c7d8ceb84e01c0d4ef27af8db34526ae
Franco Catena <francocatena@gmail.com>
2013-12-29 17:40:09 -0300

    Fix typo in newrelic redhat install

--- modules/newrelic/manifests/install/redhat.pp
+++ modules/newrelic/manifests/install/redhat.pp
@@ -1,7 +1,7 @@
 class newrelic::install::redhat {
   package { 'newrelic-repo':
     ensure   => present,
-    provider => 'rpm'
+    provider => 'rpm',
     source   => 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
   }
 
