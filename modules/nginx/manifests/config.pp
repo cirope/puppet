3709af1a5980f9185b54f5fcb80ae9aa01f74d17
Franco Catena <francocatena@gmail.com>
2014-01-02 12:57:12 -0300

    Use nginx http user and group in arch

---
+++ modules/nginx/manifests/config.pp
@@ -0,0 +1,54 @@
+class nginx::config {
+  $user = $::osfamily ? {
+    archlinux => 'http',
+    debian    => 'www-data',
+    redhat    => 'nginx'
+  }
+  $group = $::osfamily ? {
+    archlinux => 'http',
+    debian    => 'www-data',
+    redhat    => 'nginx'
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+    content => template('nginx/nginx.conf.erb')
+  }
+
+  file { '/etc/nginx/sites-available':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['nginx']
+  }
+
+  file { '/etc/nginx/sites-enabled':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['nginx']
+  }
+
+  if $server {
+    $template = $virtual ? {
+      virtualbox => $server,
+      default    => "${server}ssl"
+    }
+
+    nginx::vhost { $fqdn:
+      template => $template
+    }
+
+    nginx::vhost { 'default':
+      vhost  => 'default',
+      ensure => absent
+    }
+  }
+}
--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,51 +1,10 @@
 class nginx($server = undef) {
   include nginx::install
+  include nginx::config
 
   service { 'nginx':
     ensure  => running,
     enable  => true,
     require => Package['nginx']
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-    content => template('nginx/nginx.conf.erb')
-  }
-
-  file { '/etc/nginx/sites-available':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Package['nginx']
-  }
-
-  file { '/etc/nginx/sites-enabled':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Package['nginx']
-  }
-
-  if $server {
-    $template = $virtual ? {
-      virtualbox => $server,
-      default    => "${server}ssl"
-    }
-
-    nginx::vhost { $fqdn:
-      template => $template
-    }
-
-    nginx::vhost { 'default':
-      vhost  => 'default',
-      ensure => absent
-    }
   }
 }
