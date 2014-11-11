cda74a006cb27163799f1f3e30ff806515f06411
Franco Catena <francocatena@gmail.com>
2014-11-10 21:12:49 -0300

    Make RedHat 7 the default and _preferred_

--- tools/install/redhat.sh
+++ tools/install/redhat.sh
@@ -3,5 +3,5 @@
 yum -y update
 yum -y install git
 
-rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+rpm -ivh http://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm
 yum -y install puppet
