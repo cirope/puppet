8a52d51416ec731285abda0df520e08d75690482
Franco Catena <francocatena@gmail.com>
2015-02-27 15:18:16 -0300

    Use a slightly old Oracle client version... =(

--- modules/oracle/manifests/init.pp
+++ modules/oracle/manifests/init.pp
@@ -1,6 +1,6 @@
 class oracle {
-  oradb::client { '12.1.0.2_Linux-x86-64':
-    version                => '12.1.0.2',
+  oradb::client { '12.1.0.1_Linux-x86-64':
+    version                => '12.1.0.1',
     file                   => 'linuxamd64_12c_client.zip',
     oracleBase             => '/oracle',
     oracleHome             => '/oracle/product/12.1/client',
