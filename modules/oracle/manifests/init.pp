ab72599085a868bb6e95b9db88353d9af22a5f2c
Franco Catena <francocatena@gmail.com>
2015-02-27 15:15:19 -0300

    Remove last 0 on Oracle client version

--- modules/oracle/manifests/init.pp
+++ modules/oracle/manifests/init.pp
@@ -1,6 +1,6 @@
 class oracle {
-  oradb::client { '12.1.0.2.0_Linux-x86-64':
-    version                => '12.1.0.2.0',
+  oradb::client { '12.1.0.2_Linux-x86-64':
+    version                => '12.1.0.2',
     file                   => 'linuxamd64_12c_client.zip',
     oracleBase             => '/oracle',
     oracleHome             => '/oracle/product/12.1/client',
