75c60ba04b8062f37626794215a168a6259d7ce6
Franco Catena <francocatena@gmail.com>
2015-02-27 22:06:28 -0300

    Test some manual Oracle client installation...

--- modules/oracle/manifests/init.pp
+++ modules/oracle/manifests/init.pp
@@ -1,15 +1,6 @@
 class oracle {
-  oradb::client { '12.1.0.1_Linux-x86-64':
-    version                => '12.1.0.1',
-    file                   => 'linuxamd64_12c_client.zip',
-    oracleBase             => '/oracle',
-    oracleHome             => '/oracle/product/12.1/client',
-    user                   => 'oracle',
-    group                  => 'dba',
-    group_install          => 'oinstall',
-    downloadDir            => '/install',
-    remoteFile             => true,
-    puppetDownloadMntPoint => 'puppet:///modules/oradb/',
-    logoutput              => true
+  case $::osfamily {
+    redhat:    { include oracle::install::redhat }
+    default:   { fail('Unrecognized operating system for Oracle') }
   }
 }
---
+++ modules/oracle/manifests/install/redhat.pp
@@ -0,0 +1,39 @@
+class oracle::install::redhat {
+  $base_path           = '/etc/puppet/packages'
+  $basic_client_path   = "${base_path}/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm"
+  $devel_client_path   = "${base_path}/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm"
+  $sqlplus_client_path = "${base_path}/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm"
+
+  file { $basic_client_path:
+    ensure => present
+  }
+
+  file { $devel_client_path:
+    ensure => present
+  }
+
+  file { $sqlplus_client_path:
+    ensure => present
+  }
+
+  package { 'oracle-instantclient-basic':
+    provider => 'rpm',
+    ensure   => present,
+    source   => File[$basic_client_path],
+    require  => File[$basic_client_path]
+  }
+
+  package { 'oracle-instantclient-devel':
+    provider => 'rpm',
+    ensure   => present,
+    source   => File[$devel_client_path],
+    require  => File[$devel_client_path]
+  }
+
+  package { 'oracle-instantclient-sqlplus':
+    provider => 'rpm',
+    ensure   => present,
+    source   => File[$sqlplus_client_path],
+    require  => File[$sqlplus_client_path]
+  }
+}
