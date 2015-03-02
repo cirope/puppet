c7916f3060811664632163d3cf0b886aa27c5012
Franco Catena <francocatena@gmail.com>
2015-03-02 11:19:43 -0300

    Use only alien to install Oracle RPMs...

--- modules/oracle/manifests/install/debian.pp
+++ modules/oracle/manifests/install/debian.pp
@@ -6,12 +6,6 @@ class oracle::install::debian {
   $devel_client_path   = "${base_path}/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64"
   $sqlplus_client_path = "${base_path}/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64"
 
-  exec { 'alien':
-    command => "alien -i ${basic_client_path}.rpm; alien -i ${devel_client_path}.rpm; alien -i ${sqlplus_client_path}.rpm",
-    creates => "${basic_client_path}.deb",
-    require => Package['alien']
-  }
-
   package { 'alien':
     ensure => present
   }
@@ -20,24 +14,9 @@ class oracle::install::debian {
     ensure => present
   }
 
-  package { 'oracle-instantclient12.1-basic':
-    provider => 'dpkg',
-    ensure   => present,
-    source   => "${basic_client_path}.deb",
-    require  => [Exec['alien'], Package['libaio1']]
-  }
-
-  package { 'oracle-instantclient12.1-devel':
-    provider => 'dpkg',
-    ensure   => present,
-    source   => "${devel_client_path}.deb",
-    require  => [Exec['alien'], Package['oracle-instantclient12.1-basic']]
-  }
-
-  package { 'oracle-instantclient12.1-sqlplus':
-    provider => 'dpkg',
-    ensure   => present,
-    source   => "${sqlplus_client_path}.deb",
-    require  => [Exec['alien'], Package['oracle-instantclient12.1-basic']]
+  exec { 'alien':
+    command => "alien -i ${basic_client_path}.rpm; alien -i ${devel_client_path}.rpm; alien -i ${sqlplus_client_path}.rpm",
+    unless  => 'dpkg -s oracle-instantclient12.1-basic',
+    require => [Package['alien'], Package['libaio']]
   }
 }
