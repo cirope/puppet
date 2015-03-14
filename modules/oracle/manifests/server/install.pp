6acffdf498af6139c7198b595aa7e01483b5e756
Franco Catena <francocatena@gmail.com>
2015-03-14 02:47:57 -0300

    Remove no longer needed puppet mount point...

--- modules/oracle/manifests/server/install.pp
+++ modules/oracle/manifests/server/install.pp
@@ -3,20 +3,19 @@ class oracle::server::install {
 
   # this will look for /install/oracle/database/runInstaller
   oradb::installdb { '11.2.0.4_Linux-x86-64':
-    version                => '11.2.0.4',
-    file                   => 'oracle',
-    databaseType           => 'EE',
-    oracleBase             => $oracle::server::base,
-    oracleHome             => $oracle::server::home,
-    bashProfile            => true,
-    user                   => 'oracle',
-    group                  => 'dba',
-    group_install          => 'oinstall',
-    group_oper             => 'oper',
-    downloadDir            => '/install',
-    zipExtract             => false,
-    puppetDownloadMntPoint => $base_path,
-    require                => [
+    version       => '11.2.0.4',
+    file          => 'oracle',
+    databaseType  => 'EE',
+    oracleBase    => $oracle::server::base,
+    oracleHome    => $oracle::server::home,
+    bashProfile   => true,
+    user          => 'oracle',
+    group         => 'dba',
+    group_install => 'oinstall',
+    group_oper    => 'oper',
+    downloadDir   => '/install',
+    zipExtract    => false,
+    require       => [
       User['oracle'],
       Package[$oracle::server::dependencies::packages]
     ]
