4146cd0291d5f3c563cd1c5feb73593b4c23cf28
Franco Catena <francocatena@gmail.com>
2015-03-14 02:07:09 -0300

    Remove package file _creation_...

--- modules/oracle/manifests/client/install/redhat.pp
+++ modules/oracle/manifests/client/install/redhat.pp
@@ -6,36 +6,24 @@ class oracle::client::install::redhat {
   $devel_client_path   = "${base_path}/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm"
   $sqlplus_client_path = "${base_path}/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm"
 
-  file { $basic_client_path:
-    ensure => present
-  }
-
-  file { $devel_client_path:
-    ensure => present
-  }
-
-  file { $sqlplus_client_path:
-    ensure => present
-  }
-
   package { 'oracle-instantclient12.1-basic':
     provider => 'rpm',
     ensure   => present,
     source   => $basic_client_path,
-    require  => [File[$basic_client_path], Package['libaio']]
+    require  => Package['libaio']
   }
 
   package { 'oracle-instantclient12.1-devel':
     provider => 'rpm',
     ensure   => present,
     source   => $devel_client_path,
-    require  => [File[$devel_client_path], Package['oracle-instantclient12.1-basic']]
+    require  => Package['oracle-instantclient12.1-basic']
   }
 
   package { 'oracle-instantclient12.1-sqlplus':
     provider => 'rpm',
     ensure   => present,
     source   => $sqlplus_client_path,
-    require  => [File[$sqlplus_client_path], Package['oracle-instantclient12.1-basic']]
+    require  => Package['oracle-instantclient12.1-basic']
   }
 }
