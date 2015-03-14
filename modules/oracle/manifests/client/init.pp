39f4121b3d22e04beae8e000cb5dae9d16a7ff96
Franco Catena <francocatena@gmail.com>
2015-03-13 23:44:48 -0300

    Move Oracle client to his own class...

---
+++ modules/oracle/manifests/client/init.pp
@@ -0,0 +1,7 @@
+class oracle::client {
+  case $::osfamily {
+    debian:  { include oracle::install::debian }
+    redhat:  { include oracle::install::redhat }
+    default: { fail('Unrecognized operating system for Oracle') }
+  }
+}
--- modules/oracle/manifests/init.pp
+++ modules/oracle/manifests/init.pp
@@ -1,7 +1,3 @@
 class oracle {
-  case $::osfamily {
-    debian:  { include oracle::install::debian }
-    redhat:  { include oracle::install::redhat }
-    default: { fail('Unrecognized operating system for Oracle') }
-  }
+  include oracle::client
 }
