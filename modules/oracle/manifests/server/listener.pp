49f97d21b16287e151ed2719e134f7b1f6ac90b7
Franco Catena <francocatena@gmail.com>
2015-03-15 18:42:56 -0300

    Use variables in Oracle listener declaration...

--- modules/oracle/manifests/server/listener.pp
+++ modules/oracle/manifests/server/listener.pp
@@ -1,8 +1,8 @@
 class oracle::server::listener {
   db_listener { 'listener':
     ensure          => 'running',
-    oracle_base_dir => '/oracle',
-    oracle_home_dir => '/oracle/product/11.2/db',
+    oracle_base_dir => $oracle::server::base,
+    oracle_home_dir => $oracle::server::home,
     os_user         => $oracle::server::user,
     require         => Oradb::Installdb["${oracle::server::version}_Linux-x86-64"]
   }
