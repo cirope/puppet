9a88336aec3c6e83150b794f04951e3689543518
Franco Catena <francocatena@gmail.com>
2015-03-15 12:09:50 -0300

    Add Oracle listener as a service...

--- modules/oracle/manifests/server/listener.pp
+++ modules/oracle/manifests/server/listener.pp
@@ -6,4 +6,17 @@ class oracle::server::listener {
     os_user         => $oracle::server::user,
     require         => Oradb::Installdb["${oracle::server::version}_Linux-x86-64"]
   }
+
+  service { 'oracle_lsnr':
+    enable  => true,
+    require => File['/etc/init.d/oracle_lsnr']
+  }
+
+  file { '/etc/init.d/oracle_lsnr':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('oracle/init.sh.erb')
+  }
 }
