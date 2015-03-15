750d76beab27899c6e8c629d1f0b4f1ffe36f384
Franco Catena <francocatena@gmail.com>
2015-03-14 23:40:12 -0300

    Get user from variable in oracle::server::listener

--- modules/oracle/manifests/server/listener.pp
+++ modules/oracle/manifests/server/listener.pp
@@ -3,7 +3,7 @@ class oracle::server::listener {
     ensure          => 'running',
     oracle_base_dir => '/oracle',
     oracle_home_dir => '/oracle/product/11.2/db',
-    os_user         => 'oracle',
+    os_user         => $oracle::server::user,
     require         => Oradb::Installdb["${oracle::server::version}_Linux-x86-64"]
   }
 }
