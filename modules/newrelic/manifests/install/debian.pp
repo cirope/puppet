88a692ff1fef87fd4d565f49de285ebff0c61be3
Franco Catena <francocatena@gmail.com>
2015-09-06 16:34:28 -0300

    New APT syntax

--- modules/newrelic/manifests/install/debian.pp
+++ modules/newrelic/manifests/install/debian.pp
@@ -1,13 +1,17 @@
 class newrelic::install::debian {
   apt::source { 'newrelic':
-    ensure      => present,
-    location    => 'http://apt.newrelic.com/debian/',
-    release     => 'newrelic',
-    repos       => 'non-free',
-    key         => '548C16BF',
-    key_source  => 'http://download.newrelic.com/548C16BF.gpg',
-    include_src => false,
-    require     => Package['wget']
+    ensure   => present,
+    location => 'http://apt.newrelic.com/debian/',
+    release  => 'newrelic',
+    repos    => 'non-free',
+    require  => Package['wget'],
+    key      => {
+      id     => '548C16BF',
+      source => 'http://download.newrelic.com/548C16BF.gpg'
+    },
+    include  => {
+      src => false
+    }
   }
 
   package { 'newrelic-sysmond':
