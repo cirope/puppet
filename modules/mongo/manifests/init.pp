300ed59edbcc1f0d0d7bd0248e0c7085b605d8e2
Franco Catena <francocatena@gmail.com>
2016-02-02 13:15:11 -0300

    Renew mongodb config

--- modules/mongo/manifests/init.pp
+++ modules/mongo/manifests/init.pp
@@ -3,21 +3,21 @@ class mongo(
   $user     = $hostname,
   $password = $hostname
 ) {
-  class { 'mongodb::globals':
+  class { '::mongodb::globals':
     manage_package_repo => true
   }
 
-  class { 'mongodb::server':
+  class { '::mongodb::server':
     require => Class['mongodb::globals']
   }
 
-  class { 'mongodb::client':
+  class { '::mongodb::client':
     require => Class['mongodb::server']
   }
 
   mongodb_user { $user:
     ensure        => present,
-    password_hash => md5("${user}:mongo:${password}"),
+    password_hash => mongodb_password($user, $password),
     database      => $database,
     roles         => ['readWrite', 'dbAdmin'],
     require       => Class['mongodb::server']
