a2d94f8fc8b8555384571cacaf02f157b596a69b
Franco Catena <francocatena@gmail.com>
2016-09-30 23:01:41 -0300

    Fix sudoers file path

--- modules/user/manifests/deployer/sudoers.pp
+++ modules/user/manifests/deployer/sudoers.pp
@@ -1,5 +1,5 @@
 class user::deployer::sudoers {
-  file { "/etc/sudoers/${user::deployer::username}":
+  file { "/etc/sudoers.d/${user::deployer::username}":
     content => template('user/sudoers.erb'),
     owner   => 'root',
     group   => 'root',
