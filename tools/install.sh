1cb99b7fa72b2126d00bab4936899746bb9aa759
Franco Catena <francocatena@gmail.com>
2016-10-02 17:54:11 -0300

    Set PATH before calling update on install script

--- tools/install.sh
+++ tools/install.sh
@@ -11,12 +11,13 @@ URL=https://raw.githubusercontent.com/cirope/puppet/master/tools/install/$SCRIPT
 
 source <(wget $URL -O - 2>/dev/null || curl -L $URL)
 
-source ~/.bashrc
-
 if [ ! -d /etc/puppet/.git ]; then
   rm -rf /etc/puppet
   git clone https://github.com/cirope/puppet.git /etc/puppet
 fi
+
+PATH=$PATH:/opt/puppetlabs/bin
+export PATH
 
 source /etc/puppet/tools/update.sh
 ln -sf /etc/puppet/tools/update.sh /sbin/puppet-update
